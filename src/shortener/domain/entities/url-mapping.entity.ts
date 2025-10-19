6309711cfc935758c80d6a72743aae35d5f8be49
AdsonBruno <adsonbruno2@gmail.com>
2025-10-19 11:13:54 -0300

    feat: add the no domain method to increase the amount of access to the url

---
+++ src/shortener/domain/entities/url-mapping.entity.ts
@@ -0,0 +1,59 @@
+export interface UrlMappingProps {
+  id: string;
+  originalUrl: string;
+  shortUrlKey: string;
+  userId: string;
+  accessCount: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+}
+
+export class UrlMapping {
+  public readonly props: UrlMappingProps;
+
+  constructor(props: UrlMappingProps) {
+    this.props = props;
+  }
+
+  public static create(
+    props: Omit<
+      UrlMappingProps,
+      'accessCount' | 'createdAt' | 'updatedAt' | 'deletedAt'
+    >,
+  ): UrlMapping {
+    const now = new Date();
+
+    return new UrlMapping({
+      ...props,
+      accessCount: 0,
+      createdAt: now,
+      updatedAt: now,
+      deletedAt: null,
+    });
+  }
+
+  public static hydrate(props: UrlMappingProps): UrlMapping {
+    return new UrlMapping(props);
+  }
+
+  public get id(): string {
+    return this.props.id;
+  }
+
+  public get originalUrl(): string {
+    return this.props.originalUrl;
+  }
+
+  public get shortUrlKey(): string {
+    return this.props.shortUrlKey;
+  }
+
+  public incrementAccessCount(): UrlMapping {
+    const newProps = { ...this.props };
+    newProps.accessCount++;
+    newProps.updatedAt = new Date();
+
+    return new UrlMapping(newProps);
+  }
+}
