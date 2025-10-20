d5cf4ad70d26a0b967e168b91fd77c8fa909c21d
AdsonBruno <adsonbruno2@gmail.com>
2025-10-20 09:32:39 -0300

    feat: implement url mapping repository with typeorm

---
+++ src/shortener/infrastructure/persistence/typeorm/schemas/url-mapping.schema.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('url_mappings')
+export class UrlMappingSchema {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column()
+  originalUrl: string;
+
+  @Column({ unique: true })
+  shortUrlKey: string;
+
+  @Column({ type: 'uuid', nullable: true })
+  userId: string | null;
+
+  @Column({ default: 0 })
+  accessCount: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+}
--- src/shortener/shortner.module.ts
+++ src/shortener/shortner.module.ts
@@ -2,15 +2,23 @@ import { Module } from '@nestjs/common';
 import { CreateShortUrlUseCase } from './application/use-cases/create-short-url.use-case';
 import { ID_GENERATOR } from './application/ports/id-generator.interface';
 import { CryptoIdGeneratorService } from './infrastructure/services/crypto-id-generator.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UrlMappingSchema } from './infrastructure/persistence/typeorm/schemas/url-mapping.schema';
+import { URL_MAPPING_REPOSITORY } from './domain/repositories/url-mapping.repository';
+import { UrlMappingTypeOrmRepository } from './infrastructure/persistence/typeorm/repositories/url-mapping-typeorm.repository';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([UrlMappingSchema])],
   controllers: [],
   providers: [
     CreateShortUrlUseCase,
     {
       provide: ID_GENERATOR,
       useClass: CryptoIdGeneratorService,
+    },
+    {
+      provide: URL_MAPPING_REPOSITORY,
+      useClass: UrlMappingTypeOrmRepository,
     },
   ],
 })
