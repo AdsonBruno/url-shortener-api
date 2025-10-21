00b6135d9a72c0763cddde920cdf02c69a524c2e
AdsonBruno <adsonbruno2@gmail.com>
2025-10-21 01:41:30 -0300

    feat: improve controller with proper HTTP status codes

--- src/shortener/presentation/http/shortner.controller.ts
+++ src/shortener/presentation/http/shortner.controller.ts
@@ -1,18 +1,52 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpStatus,
+  HttpCode,
+  UseFilters,
+  Get,
+  Redirect,
+} from '@nestjs/common';
 import { CreateShortUrlUseCase } from 'src/shortener/application/use-cases/create-short-url.use-case';
 import { CreateShortUrlDto } from 'src/shortener/application/dtos/create-short-url.dto';
+import { CreateShortUrlResponseDto } from 'src/shortener/application/dtos/create-short-url-response.dto';
+import { ShortenerExceptionFilter } from '../filters/shortener-exception.filter';
 import { UrlMapping } from 'src/shortener/domain/entities/url-mapping.entity';
 
 @Controller()
+@UseFilters(ShortenerExceptionFilter)
 export class ShortenerController {
   constructor(private readonly createShortUrlUseCase: CreateShortUrlUseCase) {}
 
   @Post('shorten')
+  @HttpCode(HttpStatus.CREATED)
   async createShortUrl(
     @Body() createShortUrlDto: CreateShortUrlDto,
-  ): Promise<UrlMapping> {
-    const result = await this.createShortUrlUseCase.execute(createShortUrlDto);
+  ): Promise<CreateShortUrlResponseDto> {
+    const urlMapping =
+      await this.createShortUrlUseCase.execute(createShortUrlDto);
 
-    return result;
+    return this.mapToResponseDto(urlMapping);
+  }
+
+  @Get(':shortUrlKey')
+  @Redirect()
+  redirectToOriginalUrl() {
+    return {
+      url: 'https://example.com',
+      statusCode: HttpStatus.MOVED_PERMANENTLY,
+    };
+  }
+
+  private mapToResponseDto(urlMapping: UrlMapping): CreateShortUrlResponseDto {
+    return {
+      id: urlMapping.props.id,
+      originalUrl: urlMapping.props.originalUrl,
+      shortUrl: `http://localhost:3000/${urlMapping.props.shortUrlKey}`,
+      shortUrlKey: urlMapping.props.shortUrlKey,
+      userId: urlMapping.props.userId || undefined,
+      createdAt: urlMapping.props.createdAt,
+    };
   }
 }
