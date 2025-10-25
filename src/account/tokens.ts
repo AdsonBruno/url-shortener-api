7f98bfc61b214a5da36cea3d08ffe4336968bcf3
AdsonBruno <adsonbruno2@gmail.com>
2025-10-25 12:27:31 -0300

    feat: add IUuidLibrary abstraction and NodeCrypto adapter for dependency inversion

--- src/account/tokens.ts
+++ src/account/tokens.ts
@@ -2,3 +2,4 @@ export const USER_REPOSITORY = Symbol('USER_REPOSITORY');
 export const PASSWORD_HASHER = Symbol('PASSWORD_HASHER');
 export const ID_GENERATOR = Symbol('ID_GENERATOR');
 export const CRYPTO_LIBRARY = Symbol('CRYPTO_LIBRARY');
+export const UUID_LIBRARY = Symbol('UUID_LIBRARY');
