7fdf60c9119502dc2395c6f7637ad48efc316f3d
AdsonBruno <adsonbruno2@gmail.com>
2025-10-25 11:53:22 -0300

    feat: configure AccountModule with dependency injection and integrate with main app

---
+++ src/account/account.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AccountController } from './presentation/http/account.controller';
+import { CreateAccountUseCase } from './application/use-cases/create-account.use-case';
+import { InMemoryUserRepository } from './infrastructure/persistence/in-memory-user.repository';
+import { BcryptPasswordHasher } from './infrastructure/services/bcrypt-password-hasher.service';
+import { CryptoIdGeneratorService } from './infrastructure/services/crypto-id-generator.service';
+import { USER_REPOSITORY, PASSWORD_HASHER, ID_GENERATOR } from './tokens';
+
+@Module({
+  imports: [],
+  controllers: [AccountController],
+  providers: [
+    CreateAccountUseCase,
+    {
+      provide: USER_REPOSITORY,
+      useClass: InMemoryUserRepository,
+    },
+    {
+      provide: PASSWORD_HASHER,
+      useClass: BcryptPasswordHasher,
+    },
+    {
+      provide: ID_GENERATOR,
+      useClass: CryptoIdGeneratorService,
+    },
+  ],
+  exports: [],
+})
+export class AccountModule {}
