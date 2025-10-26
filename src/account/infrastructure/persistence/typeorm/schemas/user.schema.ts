ad46fb5978f26d4dcf4cd71ae8a7eaef89b09470
AdsonBruno <adsonbruno2@gmail.com>
2025-10-26 17:37:06 -0300

    feat: add TypeORM infrastructure for User persistence

---
+++ src/account/infrastructure/persistence/typeorm/schemas/user.schema.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class UserSchema {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+}
