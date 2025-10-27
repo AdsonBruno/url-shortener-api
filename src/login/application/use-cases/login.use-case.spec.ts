4047feb0ddc6ee6e728a709c27acb986ecd216ec
AdsonBruno <adsonbruno2@gmail.com>
2025-10-27 09:29:04 -0300

    test: should throw error when email is empty

---
+++ src/login/application/use-cases/login.use-case.spec.ts
@@ -0,0 +1,69 @@
+import { LoginUseCase } from './login.use-case';
+import { LoginDto } from '../dtos/login.dto';
+import { IUserRepository } from '../../../account/domain/repositories/user.repository';
+import { IPasswordHasher } from '../../../account/application/ports/password-hasher.interface';
+import { User } from '../../../account/domain/entities/user.entity';
+
+describe('LoginUseCase', () => {
+  class UserRepositoryStub implements IUserRepository {
+    async findByEmail(_: string): Promise<User | null> {
+      const user = User.create({
+        id: 'valid_user_id',
+        email: 'valid_email@mail.com',
+        password: 'hashed_password',
+      });
+      return Promise.resolve(user);
+    }
+
+    async findById(_: string): Promise<User | null> {
+      return Promise.resolve<User | null>(null);
+    }
+
+    async save(_: User): Promise<void> {
+      return Promise.resolve();
+    }
+  }
+
+  class PasswordHasherStub implements IPasswordHasher {
+    async hash(_: string): Promise<string> {
+      return Promise.resolve('hashed_password');
+    }
+
+    async compare(_: string, __: string): Promise<boolean> {
+      return Promise.resolve(true);
+    }
+  }
+
+  interface SutTypes {
+    sut: LoginUseCase;
+    userRepositoryStub: IUserRepository;
+    passwordHasherStub: IPasswordHasher;
+  }
+
+  const makeSut = (): SutTypes => {
+    const userRepositoryStub = new UserRepositoryStub();
+    const passwordHasherStub = new PasswordHasherStub();
+    const sut = new LoginUseCase(userRepositoryStub, passwordHasherStub);
+
+    return {
+      sut,
+      userRepositoryStub,
+      passwordHasherStub,
+    };
+  };
+
+  describe('Input Validation', () => {
+    it('should throw error when email is empty', async () => {
+      const { sut } = makeSut();
+
+      const invalidInput: LoginDto = {
+        email: '',
+        password: 'valid_password',
+      };
+
+      const promise = sut.execute(invalidInput);
+
+      await expect(promise).rejects.toThrow('Email is required');
+    });
+  });
+});
