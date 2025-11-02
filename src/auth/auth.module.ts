ada5b0701f163e8094cee2c5d6593a6843aad3b9
AdsonBruno <adsonbruno2@gmail.com>
2025-11-02 12:20:26 -0300

    feat: configure PassportModule and JwtStrategy in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { JwtTokenService } from './infrastructure/services/jwt-token.service';
+import { JwtStrategy } from './infrastructure/strategies/jwt.strategy';
 import { TOKEN_SERVICE } from './tokens';
 
 @Module({
   imports: [
     ConfigModule,
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -19,6 +22,7 @@ import { TOKEN_SERVICE } from './tokens';
     }),
   ],
   providers: [
+    JwtStrategy,
     {
       provide: TOKEN_SERVICE,
       useClass: JwtTokenService,
@@ -26,4 +30,4 @@ import { TOKEN_SERVICE } from './tokens';
   ],
   exports: [TOKEN_SERVICE],
 })
-export class AuthModule {}
+export class AuthModule { }
