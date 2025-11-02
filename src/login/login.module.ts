5db81253f4fa645beb673054bf12f3751f7bc704
AdsonBruno <adsonbruno2@gmail.com>
2025-11-02 11:40:20 -0300

    feat: import AuthModule in LoginModule

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LoginController } from './presentation/http/login.controller';
 import { LoginUseCase } from './application/use-cases/login.use-case';
 import { AccountModule } from '../account/account.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [AccountModule],
+  imports: [AccountModule, AuthModule],
   controllers: [LoginController],
   providers: [LoginUseCase],
   exports: [],
